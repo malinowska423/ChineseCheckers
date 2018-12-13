0c73941bfede28a522607a232b44a75524212393
malinowska423 <malinowska423@gmail.com>
2018-12-13 14:57:09 +0100

    loadBoard method added

--- src/main/java/com/client/Room.java
+++ src/main/java/com/client/Room.java
@@ -1,7 +1,17 @@
 package com.client;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+
 public class Room {
+    @FXML
+    GridPane boardGrid;
     public static void endGame(){
 
+    }
+
+    public void loadBoard(int playerId, String board) {
+        BoardBuilder boardBuilder = new BasicBoardBuilder();
+        boardGrid = boardBuilder.buildBoard(playerId, board);
     }
 }
